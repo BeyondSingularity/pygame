b4da9d34b910335ab53b07f5a399b420deee6058
p1tu4 <57489760+p1tu4@users.noreply.github.com>
2020-01-28 17:00:23 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,70 +1,18 @@
 import pygame
 import random
 from random import randint
+import time
+import gameUp1
 
 pygame.init()
 
-size = width, height = 800, 600
-
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+size = WIDTH, HEIGHT = pygame.display.get_surface().get_size()
 
 running = True
 all_sprites = pygame.sprite.Group()
 player_sprite = pygame.sprite.Group()
-
-
-class EnemyG2:
-    def __init__(self, radius):
-        self.speed = random.randrange(1, 4)
-        self.radius = radius
-        self.x = width - radius
-        self.y = random.randint(radius + 50, height - radius)
-
-    def create(self):
-        pygame.draw.circle(screen, (0, 0, 255), (self.x, self.y), self.radius, 0)
-        self.x -= self.speed
-
-
-class PlayerG2(pygame.sprite.Sprite):
-    def __init__(self, speed, radius):
-        super().__init__(all_sprites)
-        self.speed = speed
-        self.radius = radius
-        self.x = self.radius
-        self.y = (height - 50) // 2
-
-    def update(self):
-        pygame.draw.circle(screen, (0, 255, 0), (self.x, self.y), self.radius, 0)
-
-
-def Game2():
-    player = PlayerG2(50, 25)
-    enemy = EnemyG2(12)
-    run = True
-
-    music_Game2 = pygame.mixer.music
-    music_Game2.load('data\Game2.mp3')
-    music_Game2.play()
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                is_Exit = ask_exit()
-                if is_Exit:
-                    music_Game2.stop()
-                    run = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP and player.y - player.speed >= 50:
-                    player.y -= player.speed
-                elif event.key == pygame.K_DOWN and player.y + player.speed <= height:
-                    player.y += player.speed
-
-        screen.fill((0, 0, 0))
-
-        pygame.draw.line(screen, (255, 0, 0), (0, 50), (width, 50))
-        player.update()
-        enemy.create()
-        pygame.display.flip()
-
+FPS = 60
 
 def ask_exit():
     def dont():
@@ -72,14 +20,14 @@ def ask_exit():
         ans = ['нет', 'да']
 
         txt = font.render('Вы действительно хотите выйти?', 1, (100, 255, 100))
-        txt_x = width // 2 - txt.get_width() // 2
-        txt_y = 0
+        txt_x = WIDTH // 2 - txt.get_width() // 2
+        txt_y = 10
         screen.blit(txt, (txt_x, txt_y))
 
         for i in range(2):
             Distance = 100
             text = font.render(ans[i], 1, (100, 255, 100))
-            text_x = width // 2 - text.get_width() // 2
+            text_x = WIDTH // 2 - text.get_width() // 2
             text_y = Distance * (i + 1)
             text_w = text.get_width()
             text_h = text.get_height()
@@ -119,8 +67,8 @@ class MainMenu:
         self.list_of_games = []
         self.list_of_games_full = []
 
-        self.list_of_games.append('Ryder')
-        self.list_of_games.append('Game_2')
+        self.list_of_games.append('Игра')
+        self.list_of_games.append('Настройки')
         self.list_of_games.append('Выйти из игры')
 
     def draw(self):
@@ -130,7 +78,7 @@ class MainMenu:
         for i in range(len(self.list_of_games)):
             S = 100
             text = font.render(self.list_of_games[i], 1, (100, 255, 100))
-            self.text_x = width // 2 - text.get_width() // 2
+            self.text_x = WIDTH // 2 - text.get_width() // 2
             self.text_y = S * (i + 1)
             self.text_w = text.get_width()
             self.text_h = text.get_height()
@@ -152,12 +100,66 @@ class MainMenu:
                 self.list_of_games_full[i][1] = 0
 
 
+def Settings():
+    global cof_prefix, FPS
+    def DrawSettings():
+        font = pygame.font.Font(None, 50)
+        main_settings = ['Текстуры', 'Назад']
+        prefix_graph = ['низкие', 'средние', 'высокие', 'ультра высокие']
+        txt = font.render('Настройки', 1, (100, 255, 100))
+        txt_x = WIDTH // 2 - txt.get_width() // 2
+        txt_y = 10
+        screen.blit(txt, (txt_x, txt_y))
+
+        for i in range(len(main_settings)):
+            Distance = 100
+            if main_settings[i] == 'Текстуры':
+                text = font.render(main_settings[i] + ': ' + prefix_graph[cof_prefix], 1, (100, 255, 100))
+            else:
+                text = font.render(main_settings[i], 1, (100, 255, 100))
+            text_x = WIDTH // 2 - text.get_width() // 2
+            text_y = Distance * (i + 1)
+            text_w = text.get_width()
+            text_h = text.get_height()
+            screen.blit(text, (text_x, text_y))
+            res = main_settings[i]
+            main_settings[i] = (res, 0, text_x, text_y, text_w, text_h)
+        pygame.draw.polygon(screen, (100, 255, 100),
+                            ((main_settings[num][2] - 20, main_settings[num][3]),
+                             (main_settings[num][2] - 10,
+                              main_settings[num][3] + main_settings[num][5] // 2),
+                             (main_settings[num][2] - 20,
+                              main_settings[num][3] + main_settings[num][5])))
+
+    num = 0
+    is_run = True
+    while is_run:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    cof_prefix = (cof_prefix + 1) % 4
+                elif event.key == pygame.K_LEFT:
+                    cof_prefix = (cof_prefix - 1) % 4
+                elif event.key == pygame.K_UP:
+                    num = (num + 1) % 2
+                elif event.key == pygame.K_DOWN:
+                    num = (num - 1) % 2
+                elif event.key == pygame.K_KP_ENTER and num == 1:
+                    return False
+        FPS = 120 - cof_prefix * 35
+        screen.fill((0, 0, 0))
+        DrawSettings()
+        pygame.display.flip()
+
+
 menu = MainMenu()
 music = pygame.mixer.music
 music.load('data\MenuSong.mp3')  # Загрузка музыки
 music.play()  # начало воспроизведения музыки
 cof_limit = len(menu.list_of_games)
 cof = 1
+cof_prefix = 0
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -169,12 +171,10 @@ while running:
                 cof = (cof + 1) % cof_limit
             elif event.key == pygame.K_KP_ENTER:
                 if cof == 0:
-                    pass
-                elif cof == 1:
                     music.stop()
-                    Game2()
-                    music.load('MenuSong.mp3')
-                    music.play()
+                    gameUp1.game()
+                elif cof == 1:
+                    Settings()
                 elif cof == 2:
                     if ask_exit():
                         quit()
@@ -183,5 +183,6 @@ while running:
     menu.draw()
     menu.correct_game(cof)
     pygame.display.flip()
+    time.sleep(1 / FPS)
 
 pygame.quit()
